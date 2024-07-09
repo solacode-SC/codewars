84e238e025a3a4f2610d69e3ba2771bb8541ae3b
solacode <solacode@solacode>
2024-07-09 03:09:58 -0400

    fix some files

--- binary_array_to_number.py
+++
@@ -1,4 +0,0 @@
-def binary_array_to_number(arr):
-  return int(''.join(map(str, arr)), 2)
-
-print(binary_array_to_number([0, 1, 0, 1]))
--- toCamel.py
+++
@@ -1,8 +0,0 @@
-def to_camel_case(text):
-    newText = ""
-    for i in range(len(text)):
-        if text[i] == "-":
-            newText += text[i+1].upper()
-        else:
-            newText += text[i]
-    return newText
